8bba93146750871e723c30689e0770ec7f68c4df
brianotoole <botoole518@gmail.com>
2016-09-01 00:04:39 -0400

    single event sidebar fix

--- wp-content/themes/mjpaa/sidebar.php
+++ wp-content/themes/mjpaa/sidebar.php
@@ -14,7 +14,7 @@
 	  <!-- Event & Performances Single Sidebar -->
 	  <?php if ( is_singular('post') && get_field('event_button_text') || is_singular('ai1ec_event') && get_field('event_button_text') ) :?>
 	    <a href="<?php the_field('event_registration_link'); ?>" class="button register" target="_blank"><?php the_field('event_button_text'); ?></a>
-	  <?php elseif ( is_singular('post') || is_singular('ai1ec_event') && !get_field('event_button_text') ) :?>
+	  <?php elseif ( is_singular('event') || is_singular('ai1ec_event') && !get_field('event_button_text') ) :?>
 	    <h4 class="latest-title">News &amp; Events</h4>
 	    <?php get_template_part( 'parts/part', 'latest_news' ); ?>
 	  <?php endif ; ?>
